6082a97a624ffe098a9b4410ae2577ce88fbab99
chrisdude94 <christerkodehode@gmail.com>
2023-10-23 14:29:11 +0200

    *

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './navbar.css';
+import logo from 'C:\Users\Instruktor\Desktop\Koding\test\react-project\src\assets/logo.jpg'
+
+
+const Navbar = () => {
+    return {
+        <nav className="navbar">
+            <img src={logo} alt="Logo" className='logo'>
+            <div className="desktopMenu">
+            
+            </div>
+            /*</img><button className="desktopMenuBtn">
+                img src="" alt="" className="desktopMenuImg" />Contact Me</button>
+            */</nav>
+        )
+    }    
+
+export default Navbar;
